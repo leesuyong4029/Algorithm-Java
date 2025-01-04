dda954548329528a2ab5b179b51def208a99ca1c
suyong <edgarlee9364@gmail.com>
2025-01-04 23:02:29 +0900

    feat: 프로그래머스 스택 올바른 괄호 Level 2

---
+++ src/Programmers/Stack/Stack1.java
@@ -0,0 +1,21 @@
+package Programmers.Stack;
+
+import java.util.ArrayDeque;
+
+public class Stack1 {
+    boolean solution(String s) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+
+        char[] a = s.toCharArray();
+        for (char c : a) {
+            if (c == '(') {
+                stack.push(c);
+            }
+            else {
+                if (stack.isEmpty() || stack.pop() == c)
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
