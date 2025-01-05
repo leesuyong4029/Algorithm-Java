c41f57ab1cdafdc4a191c4a1f9c765004db88f5b
suyong <edgarlee9364@gmail.com>
2025-01-05 16:46:11 +0900

    feat: 프로그래머스 짝 지어 제거하기 Level 2 스택

---
+++ src/Programmers/Stack/Stack4.java
@@ -0,0 +1,21 @@
+package Programmers.Stack;
+
+import java.util.Stack;
+
+public class Stack4 {
+    public int solution(String s)
+    {
+        Stack<Character> stack = new Stack();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            }
+            else {
+                stack.push(c);
+            }
+        }
+
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
