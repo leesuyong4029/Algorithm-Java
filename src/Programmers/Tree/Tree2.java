51b0e9b1c2d99f616ef726cd178c5ea50f110c19
suyong <edgarlee9364@gmail.com>
2025-01-17 09:16:07 +0900

    feat: 프로그래머스 예상 대진표 Level 2 트리

---
+++ src/Programmers/Tree/Tree2.java
@@ -0,0 +1,15 @@
+package Programmers.Tree;
+
+public class Tree2 {
+    public int solution(int n, int a, int b)
+    {
+        int answer;
+
+        for (answer = 0; a != b; answer++) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+        }
+
+        return answer;
+    }
+}
