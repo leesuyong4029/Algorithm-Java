6260901dc0354c4f3dcc59e43c6b16e893b12a8a
suyong <edgarlee9364@gmail.com>
2025-01-24 23:22:55 +0900

    feat: 프로그래머스 간단한 유니온-파인드 알고리즘 구현 집합

--- src/Programmers/Set/Set1.java
+++ src/Programmers/Set/Set1.java
@@ -3,24 +3,29 @@ package Programmers.Set;
 import java.util.ArrayList;
 
 public class Set1 {
+    // 부모 저장을 위한 배열
     private static int[] parent;
 
+    // 루트 노드를 찾는 메서드
     private static int find(int x) {
+        // 만약 x의 부모가 자기 자신이면, 즉, x가 루트 노드라면 x를 반환
         if (parent[x] == x)
             return x;
+        // 그렇지 않다면 x의 부모를 찾아서 parent[x]에 저장
         parent[x] = find(parent[x]);
         return parent[x];
     }
 
     private static void union(int x, int y) {
-        int root1 = find(x);
-        int root2 = find(y);
+        int root1 = find(x); // x가 속한 집합의 루트 노드 찾기
+        int root2 = find(y); // y가 속한 집합의 루트 노드 찾기
         parent[root2] = root1;
     }
 
     private static Boolean[] solution(int k, int[][] operation) {
+        // 노드의 수 만큼 배열 생성
         parent = new int[k];
-
+        // 처음에는 각 노드가 자기 자신을 부모로 가지도록 초기화
         for (int i = 0; i < k; i++) {
             parent[i] = i;
         }
@@ -35,6 +40,7 @@ public class Set1 {
                 answer.add(find(op[1]) == find(op[2]));
             }
         }
+
         return answer.toArray(new Boolean[0]);
     }
 }
