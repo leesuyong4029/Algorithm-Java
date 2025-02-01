c57bacc90327adfb32e4f63552a278b376f7fc41
suyong <edgarlee9364@gmail.com>
2025-02-01 20:50:05 +0900

    feat: 프로그래머스 네트워크 Level 3 그래프(DFS)

---
+++ src/Programmers/Graph/Graph5.java
@@ -0,0 +1,29 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java
+package Programmers.Graph;
+
+public class Graph5 {
+    static boolean visit[];
+
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visit = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (visit[i] == false) {
+                answer++;
+                DFS(i, computers, n);
+            }
+        }
+        return answer;
+    }
+
+    static void DFS(int i, int computers[][], int n) {
+        visit[i] = true;
+
+        for (int j = 0; j < n; j++) {
+            if (visit[j] == false && computers[i][j] == 1) {
+                DFS(j, computers, n);
+            }
+        }
+    }
+}
