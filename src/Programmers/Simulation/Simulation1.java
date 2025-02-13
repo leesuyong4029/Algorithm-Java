a59029ce00c0a9180cc906b5effa7ac08e48f662
suyong <edgarlee9364@gmail.com>
2025-02-13 09:33:43 +0900

    feat: 프로그래머스 배열 회전하기 Level 2 시뮬레이션

---
+++ src/Programmers/Simulation/Simulation1.java
@@ -0,0 +1,29 @@
+package Programmers.Simulation;
+
+public class Simulation1 {
+    // 1. 2차원 배열을 인자로 받고, 90도 회전시키는 메서드
+    private static int[][] rotate90(int[][] arr) {
+        // 2. 배열의 크기 저장
+        int n = arr.length;
+        // 3. 배열의 크기와 동일한 2차원 배열 생성 (초깃값은 0)
+        int[][] rotatedArr = new int[n][n];
+
+        // 4. 배열을 90도 회전
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rotatedArr[j][n - i - 1] = arr[i][j];
+            }
+        }
+
+        // 5. 90도로 회전한 배열 반환
+        return rotatedArr;
+    }
+
+    private static int[][] solution(int[][] arr, int n) {
+        // 6. 90도 회전 메서드 호출
+        for (int i = 0; i < n; i++) {
+            arr = rotate90(arr);
+        }
+        return arr;
+    }
+}
